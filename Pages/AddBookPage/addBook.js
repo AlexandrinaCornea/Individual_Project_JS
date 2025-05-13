28b4c37b025a33c619bfe3368e72afb6e5183a12
Cornea Alexandrina <alexandrina.cornea05@gmail.com>
2025-05-13 14:42:53 +0300

    add style nd page and js

---
+++ Pages/AddBookPage/addBook.js
@@ -0,0 +1,7 @@
+document.getElementById('year').textContent = new Date().getFullYear();
+
+const coverImage = document.getElementById("coverImage")
+const bookTitle = document.getElementById("title")
+const bookAuthor = document.getElementById("author")
+const bookGenre = document.getElementById("genre")
+const bookNrPages = document.getElementById("pages")
